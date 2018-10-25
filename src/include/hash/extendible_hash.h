969faf84f32cf88a9bc03c7672fd820707e0d1f5
arunmoezhi <arunmoezhi@gmail.com>
2018-10-25 14:18:47 -0700

    autoformat changes only

--- src/include/hash/extendible_hash.h
+++ src/include/hash/extendible_hash.h
@@ -1,11 +1,11 @@
 /*
- * extendible_hash.h : implementation of in-memory hash table using extendible
- * hashing
- *
- * Functionality: The buffer pool manager must maintain a page table to be able
- * to quickly map a PageId to its corresponding memory location; or alternately
- * report that the PageId does not match any currently-buffered page.
- */
+* extendible_hash.h : implementation of in-memory hash table using extendible
+* hashing
+*
+* Functionality: The buffer pool manager must maintain a page table to be able
+* to quickly map a PageId to its corresponding memory location; or alternately
+* report that the PageId does not match any currently-buffered page.
+*/
 
 #pragma once
 
@@ -17,23 +17,23 @@
 
 namespace cmudb {
 
-template <typename K, typename V>
-class ExtendibleHash : public HashTable<K, V> {
-public:
-  // constructor
-  ExtendibleHash(size_t size);
-  // helper function to generate hash addressing
-  size_t HashKey(const K &key);
-  // helper function to get global & local depth
-  int GetGlobalDepth() const;
-  int GetLocalDepth(int bucket_id) const;
-  int GetNumBuckets() const;
-  // lookup and modifier
-  bool Find(const K &key, V &value) override;
-  bool Remove(const K &key) override;
-  void Insert(const K &key, const V &value) override;
+  template <typename K, typename V>
+  class ExtendibleHash : public HashTable<K, V> {
+  public:
+    // constructor
+    ExtendibleHash(size_t size);
+    // helper function to generate hash addressing
+    size_t HashKey(const K &key);
+    // helper function to get global & local depth
+    int GetGlobalDepth() const;
+    int GetLocalDepth(int bucket_id) const;
+    int GetNumBuckets() const;
+    // lookup and modifier
+    bool Find(const K &key, V &value) override;
+    bool Remove(const K &key) override;
+    void Insert(const K &key, const V &value) override;
 
-private:
-  // add your own member variables here
-};
+  private:
+    // add your own member variables here
+  };
 } // namespace cmudb
